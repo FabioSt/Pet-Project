66610d06f69897e9270d6e338592d851714a018c
Fabio Staiano <fabio.staiano@gmail.com>
2019-12-04 01:37:59 +0100

    setup

---
+++ Pet Project/Pet.swift
@@ -0,0 +1,33 @@
+//
+//  Pet.swift
+//  Pet Project
+//
+//  Created by Fabbio on 04/12/2019.
+//  Copyright © 2019 Fabbio. All rights reserved.
+//
+
+import Foundation
+import SpriteKit
+
+class Pet {
+    var petView = SKSpriteNode(imageNamed: "String")
+    var health = 0
+    var size = sizes.s
+    enum sizes {
+        case s
+        case m
+        case l
+        case xl
+    }
+    func checkSize() {
+        if size == sizes.s {
+            print("small")
+        } else if size == sizes.m {
+            print("medium")
+        } else if size == sizes.l {
+            print("large")
+        } else if size == sizes.xl {
+            print("extra large")
+        }
+    }
+}
